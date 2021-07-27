788bd9b00e7c0eb8ac6d2ef7151f9e87dc80c0f5
Gilbert Solano <gilbertj.solano04@gmail.com>
2021-07-27 14:11:46 -0700

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Gilbert Solano on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
